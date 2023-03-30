0bb6d91b7f46e3d873655e8a98e1eb7473930804
Nickunj Chopra <nickunjchopra@gmail.com>
2023-03-31 00:17:26 +0530

    Added the ability for users to change their passwords on the profile page.

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,7 +1,41 @@
-import { redirect } from "@sveltejs/kit";
+import { fail, redirect } from "@sveltejs/kit";
 
 export async function load({ locals }) {
   if(!locals.user) {
     throw redirect(303, '/');
+  }
+
+  const userRecord = await locals.pb.collection('users').getOne(locals.user.id);
+
+  return({ user: structuredClone(userRecord) });
+}
+
+export const actions = {
+  updateUser: async({ request, locals }) => {
+    const formData = await request.formData();
+    let oldPassword = formData.get('oldPassword');
+    let password = formData.get('password');
+    let passwordConfirm = formData.get('passwordConfirm');
+
+    if (password !== passwordConfirm) {
+      return fail(400, { form: 'updateUser', error: true, errorText: 'Both passwords should match.' });
+    }
+
+    if (!oldPassword || !password || !passwordConfirm) {
+      return fail(400, { form: 'updateUser', error: true, errorText: 'Please fill out all fields.' });
+    }
+
+    if (password.length < 8) {
+      return fail(400, { form: 'updateUser', error: true, errorText: 'Password needs to be at least 8 characters.' });
+    }
+
+    try {
+      await locals.pb.collection('users').update(locals.user.id, {oldPassword: oldPassword, password: password, passwordConfirm: passwordConfirm});
+      await locals.pb.collection('users').authWithPassword(locals.user.username, password);
+    } catch (e) {
+      return fail(400, { form: 'updateUser', error: true, errorText: e.response.message });
+    }
+
+    return { success: true, successText: 'User successfully updated!' };
   }
 }
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,6 +1,34 @@
 <script>
-  import GoBack from "../../components/util/GoBack.svelte";
+  import GoBack from '../../components/util/GoBack.svelte';
+  import { enhance } from '$app/forms';
+
+  export let data;
+  export let form;
 </script>
 
-<GoBack />
-<h1>This will be the profile page.</h1>
+<main class="w-96 h-[45vh]">
+  <div class="card w-full h-full m-0 bg-base-200">
+    <h2 class="card-title bg-primary px-8 py-4 text-primary-content rounded-tl-md rounded-tr-md">
+      <GoBack />
+      {data.user.username}
+    </h2>
+    <div class="py-14 px-8 overflow-auto text-md">
+      <form action="?/updateUser" method="POST" class="form-control" use:enhance>
+        <input type="password" name="oldPassword" placeholder="Old Password:" class="input w-full mb-6" />
+        <input type="password" name="password" placeholder="New Password (8+ characters):" class="input w-full mb-6" />
+        <input type="password" name="passwordConfirm" placeholder="New Password (again):" class="input w-full mb-12" />
+        <div 
+          class="{form?.error || form?.success ? 'tooltip tooltip-open' : ''}
+            {form?.error ? 'tooltip-error' : ''}
+            {form?.success ? 'tooltip-success': ''}
+            tooltip-bottom w-full block"
+            data-tip="{form?.errorText || form?.successText}">
+          <button class="btn btn-primary flex justify-between w-full">
+            Update Details
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path class="fill-primary-content" fill="currentColor" d="M4.4 19.425q-.5.2-.95-.088T3 18.5v-3.725q0-.35.2-.625t.55-.35L11 12l-7.25-1.8q-.35-.075-.55-.35T3 9.225V5.5q0-.55.45-.838t.95-.087l15.4 6.5q.625.275.625.925t-.625.925l-15.4 6.5Z"/></svg>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</main>
